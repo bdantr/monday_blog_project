3617cf7132463830ab7182d78aca92639f0a59c1
demidraksin <greatraksin@icloud.com>
2022-05-30 17:52:46 +0300

    Finishing registration

--- routes.py
+++ routes.py
@@ -57,3 +57,10 @@ def register():
 @login_required
 def support():
     return render_template('support.html')
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
